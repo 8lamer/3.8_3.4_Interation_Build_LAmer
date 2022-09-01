2c237bbbe782927541faa0951ce19f0f588a86c2
8lamer <107892085+8lamer@users.noreply.github.com>
2022-09-01 19:58:27 +1200

    Update page03_contact.php

--- page03_contact.php
+++ page03_contact.php
@@ -1,17 +1,19 @@
 <?php
 	session_start();
 	if(!isset($_SESSION['login_user'])){
-	header("location:01_loginV2.php");
+		header("location:01_loginV2.php");
 	}
 	else{
-	$User = $_SESSION['login_user'];
+		$User = $_SESSION['login_user'];
 	}
 ?>
+
 <!DOCTYPE html>
 <html lang = "eng">
 	<head>
 		<title>Sound Wave</title>
 		<meta charset = "utf-8">
+		
 		<!-- styles -->
 		<link rel = "stylesheet" type = "text/css" href = "css/fonts.css">
 		<link rel = "stylesheet" type = "text/css" href = "css/grids.css">
@@ -19,7 +21,6 @@
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-		<link href='https://fonts.googleapis.com/css?family=Alegreya' rel='stylesheet'>
 
 		<meta name = "Keywords" content = "Graeme, Music Database, Soundwave, Music, Tunes"/>
 		<meta name = "Author" content = "Liv Amer"/>
@@ -29,98 +30,136 @@
 		<!-- internal style sheet -->
 		<style>
 			h1,h2 {
-				font-family: 'Alegreya';
+				font-family: 'Helvetica', 'Arial', sans-serif;
 				font-size: 35w;
 				color: white;
 
 			}
-		</style>
+			
+			body, html {
+				height: 100%;
+				margin: 0;
 
+			}
+
+			.bg {
+				/* The image used */
+				background-image: url("images/orangebg.jpg");
+
+				/* Full height */
+				height: 100%;
+				width: 100%;
+
+				/* Center and scale the image nicely */
+				background-position: center;
+				background-repeat: no-repeat;
+				background-size: cover;
+
+			}
+		</style>
 
 	</head>
 	<body>
 
-		<!-- Navbar (sit on top) -->
+<!-- Navbar (sit on top) -->
 		<div class="w3-top">
 			<div class="w3-bar w3-white w3-padding w3-card">
+				<div class="w3-left">
 				<img src="images/placeholder.jpg" width="40" height="40">
+				</div>
 				<a href="indexV1.php" class="w3-bar-item w3-button"><b>Soundwave</b> </a>
+				
 
-				<!-- Float links to the right -->
+				<!-- Float links to the right-->
 				<div class="w3-right">
 					<a href="indexV1.php" class="w3-bar-item w3-button w3-xxlarge fa fa-home"></a>
 					<a href="playlistV1.php" class="w3-bar-item w3-button w3-xxlarge material-icons">playlist_add</a>	
 					<a href="contact.php" class="w3-bar-item w3-button w3-xxlarge material-icons" >contact_support</a>
 					<?php
-						if ($_SESSION["login_user"] == "Graeme"){
+					if ($_SESSION["login_user"] == "Graeme"){
 						echo "<a href='03_add_user(settings).php' class='w3-bar-item w3-button w3-xxlarge material-icons'>settings</a>";}
-						?>
+					?>
 					<a href="signout.php" class="w3-bar-item w3-button w3-xxlarge material-icons">logout</a>
 				</div>
 			</div>
 		</div>
 
 
-		<!-- background container; colour orange -->
-		<div class="container2" style="max-width:100%">
+
+		<div class="w3-orange container w3-image">
+			<div class="bg">
 
 
-			<div class="w3-content w3-container w3-padding-32 w3-display-middle-large w3-image">
+				<br>
+				<br>
+				<br>
+				<br>
 
-				<?php
-//Get the connection to the database in phpMyAdmin
-require_once("communityproject_mysqli.php");
-?>
 
-				<!-- Form -->
-				<form action="connect.php" method="post" class="w3-container w3-text-white w3-margin"> 
-					<h1 class="w3-center">CONTACT US</h1>
-					<hr>
+				<center>
+					<div class= "container w3-panel w3-padding-large" style="height:80%;width:80%; background-color:rgba(255, 255, 255, .2);">
 
-					<div class="w3-row w3-section">
-						<div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-user"></i></div>
-						<div class="w3-rest">
-							<input class="w3-input w3-border" id="name" name="name" type="text" placeholder= "Name">
+
+					<?php
+					//Get the connection to the database in phpMyAdmin
+					require_once("communityproject_mysqli.php");
+					?>
+
+					<!-- Form -->
+					<form action="connect.php" method="post" class="w3-container w3-text-white w3-margin"> 
+						<h1 class="w3-center">CONTACT US</h1>
+						<hr>
+
+						<div class="w3-row w3-section">
+							<div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-user"></i></div>
+							<div class="w3-rest">
+								<input class="w3-input w3-border" id="name" name="name" type="text" placeholder= "Name">
+							</div>
+						</div>		
+
+						<div class="w3-row w3-section">
+							<div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-envelope-o"></i></div>
+							<div class="w3-rest">
+								<input class="w3-input w3-border" id="email" name="email" type="text" placeholder="Email">
+							</div>
 						</div>
-					</div>		
 
-					<div class="w3-row w3-section">
-						<div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-envelope-o"></i></div>
-						<div class="w3-rest">
-							<input class="w3-input w3-border" id="email" name="email" type="text" placeholder="Email">
+						<div class="w3-row w3-section">
+							<div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-phone"></i></div>
+							<div class="w3-rest">
+								<input class="w3-input w3-border" id="phone" name="phone" placeholder="A contact number" >
+							</div>
 						</div>
-					</div>
 
-					<div class="w3-row w3-section">
-						<div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-phone"></i></div>
-						<div class="w3-rest">
-							<input class="w3-input w3-border" id="phone" name="phone" placeholder="A contact number" >
+						<div class="w3-row w3-section">
+							<div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-pencil"></i></div>
+							<div class="w3-rest">
+								<input class="w3-input w3-border" id="message" name="message" type="text" placeholder="Message">
+							</div>
 						</div>
-					</div>
 
-					<div class="w3-row w3-section">
-						<div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-pencil"></i></div>
-						<div class="w3-rest">
-							<input class="w3-input w3-border" id="message" name="message" type="text" placeholder="Message">
-						</div>
-					</div>
+						<p class="w3-center">
+							<input class="w3-button w3-section w3-green w3-ripple" type="submit" value="Submit"> 
+						</p>
+					</form>
 
-					<p class="w3-center">
-						<input class="w3-button w3-section w3-green w3-ripple" type="submit" value="Submit"> 
-					</p>
-				</form>
+				</div>
+
+				<br>
+				<br>
+				<br>
+
+		
+
+
+			</center>
+				<!-- Footer -->
+				<footer class="w3-container w3-padding-16 w3-center w3-white">
+					<p>Created by <a target="_blank" class="w3-hover-text-orange">Soundwave</a> &copy; Copyright Liv Amer 2022</p>
+					<a href="#top" class="w3-button w3-grey">To the top </a>
+				</footer>
+
 			</div>
-			<br>
-			<br>
-			<br>
-		</div>
-
-
-		<!-- Footer -->
-		<footer class="w3-bottom w3-center w3-white w3-padding-16">
-			<p>Created by <a target="_blank" class="w3-hover-text-orange">Soundwave</a> &copy; Copyright Liv Amer 2022</p>
-			<a href="#top" class="w3-button w3-black">To the top </a>
-		</footer>
-
+		</div>	
 	</body>
 </html>
