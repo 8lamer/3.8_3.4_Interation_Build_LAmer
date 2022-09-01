1b832728f796489db11efc3e2c2ebd219292035f
8lamer <107892085+8lamer@users.noreply.github.com>
2022-09-01 19:52:16 +1200

    Update 02_ShowUser.php

--- 02_ShowUser.php
+++ 02_ShowUser.php
@@ -12,7 +12,7 @@
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
-		<meta name = "Keywords" content = "Graeme, usic Database, Soundwave, Music, Tunes"/>
+		<meta name = "Keywords" content = "Graeme, Music Database, Soundwave, Music, Tunes"/>
 		<meta name = "Author" content = "Liv Amer"/>
 		<meta name = "Description" content = "Graeme's Music Database"/>
 		<meta name = "viewport" content = "width=device-width,initial-scale=1"/>
