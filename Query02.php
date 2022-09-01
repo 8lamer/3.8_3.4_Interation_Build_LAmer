5b741b2a82b8428b66540c28aeeb0a30e6ad6169
8lamer <107892085+8lamer@users.noreply.github.com>
2022-09-01 14:33:27 +1200

    Update Query02.php

--- Query02.php
+++ Query02.php
@@ -2,35 +2,35 @@
 <html lang = "eng">
 	<head>
 		<title>Sound Wave</title>
-		<meta charset = "utf-8">
+        	<meta charset = "utf-8">
+		
+		<!-- styles -->
 		<link rel = "stylesheet" type = "text/css" href = "css/fonts.css">
 		<link rel = "stylesheet" type = "text/css" href = "css/grids.css">
-		<link rel = "stylesheet" href = "css/style.css"/>
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-
-
-		<meta name = "Keywords" content = "Graeme, Music Database, Soundwave, Music, Tunes"/>
-		<meta name = "Author" content = "Liv Amer"/>
-		<meta name = "Description" content = "Graeme's Music Database"/>
-		<meta name = "viewport" content = "width=device-width,initial-scale=1"/>
-
+		
+        <meta name = "Keywords" content = "Graeme, Music Database, Soundwave, Music, Tunes"/>
+        <meta name = "Author" content = "Liv Amer"/>
+        <meta name = "Description" content = "Graeme's Music Database"/>
+        <meta name = "viewport" content = "width=device-width,initial-scale=1"/>
+        
 
 	</head>
 	<body>
-
-		<!-- Navbar (sit on top) -->
+		
+			<!-- Navbar (sit on top) -->
 		<div class="w3-top">
-			<div class="w3-bar w3-white w3-wide w3-padding w3-card">
+			<div class="w3-bar w3-grey w3-padding w3-card">
 				<img src="images/placeholder.jpg" width="40" height="40">
 
-				<a href="indexV1.php" class="w3-bar-item w3-button"><b>logo</b> </a>
+				<a href="indexV1.php" class="w3-bar-item w3-button"><b>Soundwave</b> </a>
 
 
-				<!-- Float links to the right. Hide them on small screens -->
-				<div class="w3-right w3-hide-small">
+				<!-- Float links to the right -->
+				<div class="w3-right">
 					<a href="indexV1.php" class="w3-bar-item w3-button w3-xxlarge fa fa-home"></a>
 					<a href="playlistV1.php" class="w3-bar-item w3-button w3-xxlarge material-icons">playlist_add</a>	
 					<a href="contact.php" class="w3-bar-item w3-button w3-xxlarge material-icons" >contact_support</a>
@@ -40,63 +40,54 @@
 		</div>
 
 
-
-		<br>
-		<br>
-		<br>
-		<br>	
-
+	<div class="w3-black">
+			<div class="w3-container w3-content w3-padding-32" style="max-width:100%">
+		
 		<!-- Container Queries -->
-		<div class="w3-content w3-container w3-padding-64 w3-center w3-yellow w3-image">
+		<div class="w3-content w3-container w3-padding-64 w3-center w3-black w3-image">
 			<a href="playlist_genre.php"> <img src="images/placeholder.jpg" class="w3-button w3-round w3-opacity w3-hover-opacity-off"  alt="sun" height="170" style="width:310px"> </a>
 			<a href="playlistV1.php"> <img src="images/placeholder.jpg" class="w3-button w3-round w3-opacity w3-hover-opacity-off"  alt="Song A - Z" height="170" style="width:310px"> </a>
 			<a href="playlist_SongArtist.php"> <img src="images/placeholder.jpg" class="w3-button w3-round w3-opacity w3-hover-opacity-off"  alt="sun" height="170" style="width:310px"> </a>
+		    <h3 class="w3-center w3-black"> GENRE / ARTIST A - Z</h3>
 
 
+ 
+		<heading1>
+				
+			<Song_ID1><h4>#</h4></Song_ID1>
+			<image1><h4></h4></image1>
+			<Title1><h4>Title</h4></Title1>
+			<Artist1><h4>Artist</h4></Artist1>
+			<Album1><h4>Album</h4></Album1>
+			<Genre1><h4>Genre</h4></Genre1>
+			<Size1><h4>Size</h4></Size1>
+			<Duration1><h4>Duration</h4></Duration1>
 
-			<h3 class="w3-center w3-black">GENRE A - Z</h3>
-
-
-
-			<heading1>
-
-				<Song_ID1><h4>#</h4></Song_ID1>
-				<image1><h4></h4></image1>
-				<Title1><h4>Title</h4></Title1>
-				<Artist1><h4>Artist</h4></Artist1>
-				<Album1><h4>Album</h4></Album1>
-				<Genre1><h4>Genre</h4></Genre1>
-				<Size1><h4>Size</h4></Size1>
-				<Duration1><h4>Duration</h4></Duration1>
-
-			</heading1>
-
-
-
-
-
-			<div class="overflowTest w3-center" >
-
-				<content>
-					<?php
+		</heading1>
+	
+	
+	
+		<div class="overflowTest w3-center" >
+			<content>
+				<?php
 					require "communityproject_mysqli.php";
-
+				
 					//Creates a variable to store the sql query
-					$query = ("SELECT s.Song_ID, s.Images, s.Title, 
-									GROUP_CONCAT(DISTINCT ar.Artist SEPARATOR ', ') AS 'Artist',
-									GROUP_CONCAT(DISTINCT al.Album SEPARATOR ', ') AS 'Album',
-									GROUP_CONCAT(DISTINCT g.Genre SEPARATOR ', ') AS 'Genre',
-									s.Size, s.Seconds
-									FROM songdetails AS s 
-									INNER JOIN Album al ON s.Album_PK = al.Album_PK 
-									JOIN songtoartist j ON s.Song_ID = j.Song_ID 
-									JOIN Artist ar ON ar.Artist_PK = j.Artist_PK 
-									JOIN songtogenre as gs ON s.Song_ID = gs.Song_ID
-									JOIN Genre as g ON g.Genre_PK = gs.Genre_PK
-									GROUP BY s.Song_ID 
-									ORDER BY Genre ASC, Artist ASC");
+					$query = ("SELECT s.Song_ID, s.Images, s.Title,
+								GROUP_CONCAT(DISTINCT ar.Artist SEPARATOR ', ') AS 'Artist',
+								GROUP_CONCAT(DISTINCT al.Album SEPARATOR ', ') AS 'Album',
+								GROUP_CONCAT(DISTINCT g.Genre SEPARATOR ', ') AS 'Genre',
+								s.Size, s.Seconds
+								FROM songdetails AS s 
+								INNER JOIN Album al ON s.Album_PK = al.Album_PK 
+								JOIN songtoartist j ON s.Song_ID = j.Song_ID 
+								JOIN Artist ar ON ar.Artist_PK = j.Artist_PK 
+								JOIN songtogenre as gs ON s.Song_ID = gs.Song_ID
+								JOIN Genre as g ON g.Genre_PK = gs.Genre_PK
+								GROUP BY s.Song_ID 
+								ORDER BY Genre ASC, Artist ASC");
 
-
+				
 					//Runs and stores the query using the variable $con (see nav.php) and $query
 					$result = mysqli_query($conn,$query);
 					//runs in a 'while' loop
@@ -104,44 +95,43 @@
 					{
 					?>
 					<!--php is above. HTML is below. Used to output the query results -->
+				
 
+				<heading2>
+					<space></space>
+					
+					<Song_ID2><p2 class = 'white'><?php echo $output['Song_ID']; ?></p2></Song_ID2>
+					<img src="images/<?php echo $output['Images']; ?>" style="width: 50px; height: 50px;">
+					<Title2><p2 class = 'white'><?php echo $output['Title']; ?></p2></Title2>
+					<Artist2><p2 class = 'white'><?php echo $output['Artist']; ?></p2></Artist2>
+					<Album2><p2 class = 'white'><?php echo $output['Album']; ?></p2></Album2>
+					<Genre2><p2 class = 'white'><?php echo $output['Genre']; ?></p2></Genre2>
+					<Size2><p2 class = 'white'><?php echo $output['Size']; ?></p2></Size2>
+					<Duration2><p2 class = 'white'><?php echo $output['Seconds']; ?></p2></Duration2>
+					<space></space>
+					
+				</heading2>
+				<?php
+				//Closes the output while loop
+				}
+				?>
+		
 
-					<heading2>
-						<space></space>
+			</content>
 
-						<Song_ID2><p class = 'white'><?php echo $output['Song_ID']; ?></p></Song_ID2>
-
-						<img src="images/<?php echo $output['Images']; ?>" style="width: 50px; height: 50px;">
-
-
-						<Title2><p class = 'white'><?php echo $output['Title']; ?></p></Title2>
-						<Artist2><p class = 'white'><?php echo $output['Artist']; ?></p></Artist2>
-						<Album2><p class = 'white'><?php echo $output['Album']; ?></p></Album2>
-						<Genre2><p class = 'white'><?php echo $output['Genre']; ?></p></Genre2>
-						<Size2><p class = 'white'><?php echo $output['Size']; ?></p></Size2>
-						<Duration2><p class = 'white'><?php echo $output['Seconds']; ?></p></Duration2>
-						<space></space>
-
-					</heading2>
-					<?php
-						//Closes the output while loop
-					}
-					?>
-
-
-					</content>
-
-
+	
 			</div>
 		</div>
-		
-		
-				<!-- Footer -->
-		<footer class="w3-center w3-black w3-padding-16">
-			<p1>Created by <a target="_blank" class="w3-hover-text-orange">Soundwave</a> &copy; Copyright Liv Amer 2022</p1>
-			<br>
-			<br>
-			<a href="#top" class="w3-button w3-light-grey">To the top </a>
+
+		</div>
+		</div>
+
+		<br>
+		<br>
+		<!-- Footer -->
+		<footer class="w3-bottom w3-center w3-grey w3-padding-16">
+			<p>Created by <a target="_blank" class="w3-hover-text-orange">Soundwave</a> &copy; Copyright Liv Amer 2022</p>
+			<a href="#top" class="w3-button w3-black">To the top </a>
 		</footer>
 
 
