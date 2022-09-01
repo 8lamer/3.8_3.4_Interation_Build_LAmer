f582cd52828217ab7ebf4668c8ca07f00b4e45d1
8lamer <107892085+8lamer@users.noreply.github.com>
2022-09-01 14:36:58 +1200

    Update 02_ShowUser.php

--- 02_ShowUser.php
+++ 02_ShowUser.php
@@ -11,28 +11,39 @@
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+		<link href='https://fonts.googleapis.com/css?family=Alegreya' rel='stylesheet'>
+		<link rel="stylesheet" href="https://use.typekit.net/tjv0dzy.css">
 
 		<meta name = "Keywords" content = "Graeme, Music Database, Soundwave, Music, Tunes"/>
 		<meta name = "Author" content = "Liv Amer"/>
 		<meta name = "Description" content = "Graeme's Music Database"/>
 		<meta name = "viewport" content = "width=device-width,initial-scale=1"/>
 
+		<!-- internal style sheet -->
+		<style>
+			h2 {
+				font-family: alegreya, serif;
+				font-size: 30px;
+				color: white;
+
+			}
+		</style>
 
 	</head>
 	<body>
 
 		<!-- Navbar (sit on top) -->
 		<div class="w3-top">
-			<div class="w3-bar w3-grey w3-padding w3-card">
+			<div class="w3-bar w3-white w3-padding w3-card">
 				<img src="images/placeholder.jpg" width="40" height="40">
 				<a href="indexV1.php" class="w3-bar-item w3-button"><b>Soundwave</b> </a>
 
 				<!-- Float links to the right -->
-				<div class="w3-right" >
+				<div class="w3-right">
 					<a href="indexV1.php" class="w3-bar-item w3-button w3-xxlarge fa fa-home"></a>
 					<a href="playlistV1.php" class="w3-bar-item w3-button w3-xxlarge material-icons">playlist_add</a>	
 					<a href="contact.php" class="w3-bar-item w3-button w3-xxlarge material-icons" >contact_support</a>
-					<a href="settings.php" class="w3-bar-item w3-button w3-xxlarge material-icons">settings</a>	
+					<a href="signout.php" class="w3-bar-item w3-button w3-xxlarge material-icons">logout</a>
 				</div>
 			</div>
 		</div>
@@ -55,54 +66,63 @@
 
 		<!-- Title for query: show users -->
 		<div style="margin-left:8%">
-			<div class="w3-container w3-dark-grey">
+			<div class="container3">
 				<h2>Show User</h2>
 			</div>
 
-			<br>
-			<!-- Users php -->
-			<div class="w3-row">
-				<div class="w3-third w3-center">
-					<h5><user1>USERS</user1></h5>
-					<h6>
-						<?php
-						//connect.php (tells where to connect servername, username, password, dbaseName)
-						require "communityproject_mysqli.php";
 
-						//create a variable to store sql code for the 'display all users' query
-						$query = ("SELECT * FROM users");
+			<div class="container4 w3-display-middle">
 
-						//run the query
-						$result = mysqli_query($conn,$query);
+				<div class="scroller w3-center w3-row">
+					<div class="w3-cell-row" style="width:100%">
 
-						while ($output = mysqli_fetch_array($result))
-						{
-							echo "<user2>". $output['User_ID']."</user2><br>";
-							//closes the while loop
-						}
-						?>
-					</h6>
-				</div>
+						<!-- Users php -->
+						<div class="w3-container w3-white w3-cell">
+							<p><user1>USERS</user1></p>
+
+							<h6>
+								<?php
+								//connect.php (tells where to connect servername, username, password, dbaseName)
+								require "communityproject_mysqli.php";
+
+								//create a variable to store sql code for the 'display all users' query
+								$query = ("SELECT * FROM users");
+
+								//run the query
+								$result = mysqli_query($conn,$query);
+
+								while ($output = mysqli_fetch_array($result))
+								{
+									echo "<user2>". $output['User_ID']."</user2><br>";
+									//closes the while loop
+								}
+								?>
+							</h6>
+						</div>
 
 
-				<!-- Passwords php -->
-				<div class="w3-third w3-center ">
-					<h5><password1>PASSWORDS</password1></h5>
-					<h6>
-						<?php
-						//run the query
-						$result = mysqli_query($conn,$query);
 
-						while ($output = mysqli_fetch_array($result))
-						{
-							echo "<user2>". $output['Password']."</user2><br>";
-							//closes the while loop
-						}
-						?>
-					</h6>
+						<!-- Passwords php -->
+						<div class="w3-container w3-white w3-cell">
+							<p><password1>PASSWORDS</password1></p>
+							<h6>
+								<?php
+								//run the query
+								$result = mysqli_query($conn,$query);
+
+								while ($output = mysqli_fetch_array($result))
+								{
+									echo "<user2>". $output['Password']."</user2><br>";
+									//closes the while loop
+								}
+								?>
+							</h6>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
+
 
 
 		<!-- footer (sits at bottom of page) -->
