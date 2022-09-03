fef260210dcba68a42ceb6231d16d255ee4430cc
8lamer <107892085+8lamer@users.noreply.github.com>
2022-09-03 15:59:12 +1200

    Update Playlist_Genre_Pop.php

--- Playlist_Genre_Pop.php
+++ Playlist_Genre_Pop.php
@@ -21,6 +21,8 @@ else{
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+		<!-- favicon -->
+		<link rel="icon" type="image/x-icon" href="images/favicon1.ico">
 
 		<meta name = "Keywords" content = "Graeme, Music Database, Soundwave, Music, Tunes"/>
 		<meta name = "Author" content = "Liv Amer"/>
@@ -44,7 +46,7 @@ else{
 
 			.bg {
 				/* The image used */
-				background-image: url("images/blackbg.jpg");
+				background-image: url("images/orangebg.jpg");
 
 				/* Full height */
 				height: 100%;
@@ -79,9 +81,9 @@ else{
 
 		<!-- Navbar (sit on top) -->
 		<div class="w3-top">
-			<div class="w3-bar w3-grey w3-padding w3-card">
+			<div class="w3-bar w3-white w3-padding w3-card">
 				<div class="w3-left">
-					<img src="images/placeholder.jpg" width="40" height="40">
+					<img src="images/logo.jpg" width="40" height="40">
 				</div>
 				<a href="indexV1.php" class="w3-bar-item w3-button"><b>Soundwave</b> </a>
 
@@ -112,14 +114,16 @@ else{
 				<br>
 
 				<center>
-					<div class= "w3-container w3-panel w3-padding-large" style="height:80%;width:80%; background-color:rgba(0,0,0,0.3);">
+					
+					<!-- clear container for aesthetic -->
+					<div class= "w3-container w3-panel w3-padding-large" style="height:80%;width:80%; background-color:rgba(255, 255, 255, .2);">
 
+						<!-- picture for album -->
 						<div class="w3-container" style="height:100%;width:100%;">	
-							<img class="w3-round w3-left w3-border w3-border-orange" src="images/placeholder.jpg" width="200" height="180">
+							<img class="w3-round w3-left" src="images/placeholder.jpg" width="200" height="180">
 
 							<div class="w3-container w3-left">
-
-								<!-- title for genre -->
+								<!-- title for genre + description -->
 								<h2 class="w3-left-align">POP</h2>
 								<p3 class="w3-left-align">Pop famous hits today, listen now!</p3>
 
@@ -140,8 +144,6 @@ else{
 							<Duration1><h4>Duration</h4></Duration1>
 
 						</heading1>
-
-
 
 
 
@@ -207,12 +209,21 @@ else{
 						</div>
 					</div>
 
+					<br>
+					
 				</center>	
 				<!-- Footer -->
-				<footer class="w3-container w3-padding-16 w3-center w3-grey">
+				<footer class="w3-container w3-padding-16 w3-center w3-white">
 					<p>Created by <a target="_blank" class="w3-hover-text-orange">Soundwave</a> &copy; Copyright Liv Amer 2022</p>
-					<a href="#top" class="w3-button w3-black">To the top </a>
-					
+					<a href="#top" class="w3-button w3-grey">To the top </a>
+					<div class="w3-xlarge w3-section">
+						<i class="fa fa-facebook-official w3-hover-opacity"></i>
+						<i class="fa fa-instagram w3-hover-opacity"></i>
+						<i class="fa fa-snapchat w3-hover-opacity"></i>
+						<i class="fa fa-pinterest-p w3-hover-opacity"></i>
+						<i class="fa fa-twitter w3-hover-opacity"></i>
+						<i class="fa fa-linkedin w3-hover-opacity"></i>
+					</div>
 				</footer>
 
 			</div>
