846ccb940aec970874b33caa40877c9587c7fdec
8lamer <107892085+8lamer@users.noreply.github.com>
2022-09-07 19:55:44 +1200

    Update 04_UpdateUser.php

--- 04_UpdateUser.php
+++ 04_UpdateUser.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang = "eng">
+<html lang = "en">
 	<head>
 		<title>Sound Wave</title>
 		<meta charset = "utf-8">
@@ -35,7 +35,7 @@
 		<div class="w3-top">
 			<div class="w3-bar w3-white w3-padding w3-card">
 				<div class="w3-left">
-				<img src="images/logo.jpg" width="40" height="40">
+				<img src="images/logo.jpg" alt="logo" width="40" height="40">
 				</div>
 				<a href="indexV1.php" class="w3-bar-item w3-button"><b>Soundwave</b> </a>
 
@@ -76,9 +76,9 @@
 				<div class="content"><!-- Holds the update users content -->
 
 					<form method = "post" id= "05_update_user.php">
-						<h6><label for = 'login'>Existing User Name:</label>
+						<h6><label>Existing User Name:</label>
 							<input type = "text" name = "ExistingUserName" placeholder="Enter existing user name"/></h6>
-						<h6><label for = 'login'>New User Name:</label>
+						<h6><label>New User Name:</label>
 							<input type = "text" name = "NewUserName" placeholder="Enter new user name"/></h6>
 						<h6><input class="w3-padding-small w3-button w3-green" type = "submit" value = "Update" /></h6><br>
 					</form>
@@ -109,7 +109,7 @@
 
 		<!-- Footer -->
 		<footer class="w3-bottom w3-center w3-black w3-padding-64">
-			<p1>Created by <a target="_blank" class="w3-hover-text-orange">Soundwave</a> &copy; Copyright Liv Amer 2022</p1>
+			<p1>Created by <a href= "indexV1.php" target="_blank" class="w3-hover-text-orange">Soundwave</a> &copy; Copyright Liv Amer 2022</p1>
 		</footer>
 
 
